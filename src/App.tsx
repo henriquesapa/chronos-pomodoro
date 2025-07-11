8592285664b2a87748fe1735da1d719155be1353
HenriqueSAP <hsilvaaparecido@gmail.com>
2025-07-11 16:46:00 -0400

    Criando estrutura html

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <Heading attr={12345} attr2='String'>
-        Olá mundo!
-      </Heading>
-      <body>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae
-        voluptate ducimus atque at, voluptates laudantium. Ipsa nemo distinctio
-        in quaerat tenetur corporis necessitatibus veritatis, magnam culpa
-        pariatur, tempora excepturi!
-      </body>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>TESTE</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>HORAS</section>
+        </div>
+      </div>
     </>
   );
 }
