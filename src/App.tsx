8dc88c133fdb49ac25978073520284fe2c8a0802
HenriqueSAP <hsilvaaparecido@gmail.com>
2025-07-15 08:15:52 -0400

    adicionando icones

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
@@ -11,7 +11,7 @@ export function App() {
         <Logo />
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
     </>
   );
