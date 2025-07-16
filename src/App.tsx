f547f58a308941606f68207600ec320c8e47ef58
HenriqueSAP <hsilvaaparecido@gmail.com>
2025-07-16 12:19:16 -0400

    feat:Criandoo componente footer(rodapé)

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/ DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -44,6 +45,9 @@ export function App() {
             <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+      <Container>
+        <Footer />
       </Container>
     </>
   );
