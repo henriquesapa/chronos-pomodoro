83ae74b269928724ca1ae0b4a2abde95e4fdddc8
HenriqueSAP <hsilvaaparecido@gmail.com>
2025-07-17 13:47:21 -0400

    feat: componente generico pra html

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
+import { NotFound } from './pages/NotFound';
 
 export function App() {
-  return <Home />;
+  return <NotFound />;
 }
