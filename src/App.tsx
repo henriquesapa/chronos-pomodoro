a2229247eba980b05e7b20f22de95abb4c9c980f
HenriqueSAP <hsilvaaparecido@gmail.com>
2025-07-17 23:22:47 -0400

    Definindo models de aplicação TaskModel e TaskStateModel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
-import { NotFound } from './pages/NotFound';
+import { Home } from './pages/Home';
 
 export function App() {
-  return <NotFound />;
+  return <Home />;
 }
