5ed63abb3b8b7c4b1444bd1e5aeace1890dacae4
HenriqueSAP <hsilvaaparecido@gmail.com>
2025-07-25 15:44:58 -0400

    Criando o componente Tips

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,11 +2,13 @@ import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
+import { TaskModel } from '../../models/TaskModel';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
-import type { TaskModel } from '../../models/TaskModel';
+import { DefaultButton } from '../ DefaultButton';
+import { Tips } from '../../Tips';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -43,8 +45,6 @@ export function MainForm() {
 
   function handleInterruptTask() {
     dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
-
-    dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
   return (
@@ -61,7 +61,7 @@ export function MainForm() {
       </div>
 
       <div className='formRow'>
-        <p>Próximo intervalo é de 25min</p>
+        <Tips />
       </div>
 
       {state.currentCycle > 0 && (
