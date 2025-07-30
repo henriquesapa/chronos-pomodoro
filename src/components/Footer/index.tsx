66ae549518a8183358e5c8b1f38a5546645f2b32
HenriqueSAP <hsilvaaparecido@gmail.com>
2025-07-30 15:13:57 -0400

    feat: router, iniciando o roteamento

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router';
 import styles from './styles.module.css';
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href=''>Entenda como funciona a técnica pomdoro 🍅</a>
-      <a href=''>
+      <Link to='/about-pomodoro/'>
+        Entenda como funciona a técnica pomdoro 🍅
+      </Link>
+      <a href='/'>
         Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚
       </a>
     </footer>
