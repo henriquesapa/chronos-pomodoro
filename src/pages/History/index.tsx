546f72dd02916ced60d6cbedfbe8d51fe83b1540
HenriqueSAP <hsilvaaparecido@gmail.com>
2025-07-31 11:36:58 -0400

    Criando lógica pra traduzir a tarefa

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -6,6 +6,7 @@ import { MainTemplate } from '../../templates/MainTemplate';
 import styles from './styles.module.css';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { formatDate } from '../../utils/formatDate';
+import { getTaskStatus } from '../../utils/getTaskStatus';
 export function History() {
   const { state } = useTaskContext();
   return (
@@ -37,13 +38,18 @@ export function History() {
             </thead>
             <tbody>
               {state.tasks.map(task => {
+                const taskTypeDictionary = {
+                  workTime: 'Foco',
+                  shortBreakTime: 'Descanso curto',
+                  longBreakTime: 'Descanso longo.',
+                };
                 return (
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.duration} min</td>
                     <td>{formatDate(task.startDate)}</td>
-                    <td>{task.interruptDate}</td>
-                    <td>{task.type}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{taskTypeDictionary[task.type]}</td>
                   </tr>
                 );
               })}
